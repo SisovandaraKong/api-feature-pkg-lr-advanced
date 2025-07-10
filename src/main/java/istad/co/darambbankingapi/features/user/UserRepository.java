f72574f5229f53ee36dfb4aa816b0f5c668fe60f
Kong_Sisovandara <sisovandara@gmail.com>
2025-07-11 01:45:05 +0700

    spring structure features

--- src/main/java/istad/co/darambbankingapi/features/user/UserRepository.java
+++ src/main/java/istad/co/darambbankingapi/features/user/UserRepository.java
@@ -4,5 +4,8 @@ package istad.co.darambbankingapi.features.user;
 import istad.co.darambbankingapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUuid(String uuid);
 }
